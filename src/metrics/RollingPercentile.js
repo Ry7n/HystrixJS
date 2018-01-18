9b6d53a4daf49811b455a9acb8fca06d37c93f68
Ryan Cannon <github@ryancannon.com>
2018-01-18 08:58:49 -0800

    Metrics perf improvements
    This change should improve the overall performance of HystrixJS by
    leveraging some of the quirks of the V8 runtime. Specifically:
    
    * Switching to `Map`s over dynamic objects, which is [significantly
      faster for property retrieval][perf-map]
    * Using fixed-size Arrays for buckets in order to avoid the cost of
    * Re-using buckets rather than garbage collecting and reinstantiating
      them.
    * In `RollingPercentile`, re-using a global instance of `Stats`
    
    In general, this will cause applications to pay the more memory cost of
    running HystrixJS at startup, and increase less with volume, while also
    reducing load on CPU and the garbage collector.
    
    None of these changes alter the exposed interfaces.
    
    [perf-map]: https://jsperf.com/javascript-objects-vs-map-performance
    [perf-array]: https://jsperf.com/static-vs-dynamic-array/4

--- src/metrics/RollingPercentile.js
+++ src/metrics/RollingPercentile.js
@@ -11,32 +11,29 @@ export default class RollingPercentile {
             } = {}) {
         this.windowLength = timeInMillisecond;
         this.numberOfBuckets = numberOfBuckets;
-        this.buckets = [];
+        this.bucketSizeInMilliseconds = this.windowLength / this.numberOfBuckets;
+        const buckets = this.buckets = new Array(numberOfBuckets);
+        for (let i = 0; i < numberOfBuckets; i += 1) {
+            buckets[i] = new Bucket(-Infinity);
+        }
+        this.bucketIndex = numberOfBuckets - 1;
         this.percentileSnapshot = new PercentileSnapshot();
     }
 
-    get bucketSizeInMilliseconds() {
-        return this.windowLength / this.numberOfBuckets;
-    }
-
     addValue(value) {
-        this.getCurrentBucket().addValue(value)
+        this.getCurrentBucket().addValue(value);
     }
 
     getPercentile(percentile) {
-        return this.percentileSnapshot.getPercentile(percentile);
+        // TODO: this method accepts either a string or a number, which can cause
+        // deoptimization by the complier.
+        return this.percentileSnapshot.get(`p${percentile}`);
     }
 
     getCurrentBucket() {
         let currentTime = ActualTime.getCurrentTime();
 
-        if (this.buckets.length === 0) {
-            let newBucket = new Bucket(currentTime);
-            this.buckets.push(newBucket);
-            return newBucket;
-        }
-
-        let currentBucket = this.buckets[this.buckets.length-1];
+        let currentBucket = this.buckets[this.bucketIndex];
         if (currentTime < (currentBucket.windowStart + this.bucketSizeInMilliseconds)) {
             return currentBucket;
         } else {
@@ -46,56 +43,42 @@ export default class RollingPercentile {
     }
 
     rollWindow(currentTime) {
-        let newBucket = new Bucket(currentTime);
-        if (this.buckets.length == this.numberOfBuckets) {
-            this.buckets.shift();
+        let newBucketIndex = this.bucketIndex + 1;
+        if (newBucketIndex === this.numberOfBuckets) {
+            newBucketIndex = 0;
         }
+
         this.percentileSnapshot = new PercentileSnapshot(this.buckets);
-        this.buckets.push(newBucket);
+
+        this.bucketIndex = newBucketIndex;
+        let newBucket = this.buckets[newBucketIndex];
+        newBucket.windowStart = currentTime;
+        newBucket.reset();
     }
 }
 
-class PercentileSnapshot {
-    constructor(allBuckets = []) {
-        this.stats = new Stats();
-        for (let bucket of allBuckets) {
-            this.stats.push(bucket.values)
-        }
+const stats = new Stats();
 
-        this.mean = this.stats.amean() || 0;
-        this.p0 = this.stats.percentile(0) || 0;
-        this.p5 = this.stats.percentile(5) || 0;
-        this.p10 = this.stats.percentile(10) || 0;
-        this.p25 = this.stats.percentile(25) || 0;
-        this.p50 = this.stats.percentile(50) || 0;
-        this.p75 = this.stats.percentile(75) || 0;
-        this.p90 = this.stats.percentile(90) || 0;
-        this.p95 = this.stats.percentile(95) || 0;
-        this.p99 = this.stats.percentile(99) || 0;
-        this.p995 = this.stats.percentile(99.5) || 0;
-        this.p999 = this.stats.percentile(99.9) || 0;
-        this.p100 = this.stats.percentile(100) || 0;
+function pushPercentileBucket(bucket) {
+    stats.push(bucket.values);
+}
 
-    }
-
-    getPercentile(percentile = "mean") {
-        if (percentile === "mean") {
-            return this.mean;
-        }
-
-        switch (percentile) {
-            case 0: return this.p0;
-            case 5: return this.p5;
-            case 10: return this.p10;
-            case 25: return this.p25;
-            case 50: return this.p50;
-            case 75: return this.p75;
-            case 90: return this.p90;
-            case 95: return this.p95;
-            case 99: return this.p99;
-            case 99.5: return this.p995;
-            case 99.9: return this.p999;
-            case 100: return this.p100;
-        }
-    }
+function getPercentileSnapshot(allBuckets = []) {
+    allBuckets.forEach(pushPercentileBucket);
+    const results = new Map();
+    results.set("pmean", stats.amean() || 0);
+    results.set("p0", stats.percentile(0) || 0);
+    results.set("p5", stats.percentile(5) || 0);
+    results.set("p10", stats.percentile(10) || 0);
+    results.set("p25", stats.percentile(25) || 0);
+    results.set("p50", stats.percentile(50) || 0);
+    results.set("p75", stats.percentile(75) || 0);
+    results.set("p90", stats.percentile(90) || 0);
+    results.set("p95", stats.percentile(95) || 0);
+    results.set("p99", stats.percentile(99) || 0);
+    results.set("p995", stats.percentile(99.5) || 0);
+    results.set("p999", stats.percentile(99.9) || 0);
+    results.set("p100", stats.percentile(100) || 0);
+    stats.reset();
+    return results;
 }
